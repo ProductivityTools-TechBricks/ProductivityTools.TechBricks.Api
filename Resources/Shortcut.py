83ab3a5659dd590524ff5eacaacce4c4e109eeeb
Pawel Wujczyk <pwujczyk@hotmail.com>
2022-05-12 22:52:59 +0200

    table returned

--- Resources/Shortcut.py
+++ Resources/Shortcut.py
@@ -7,7 +7,11 @@ class ShortcutResource(Resource):
     def get(self):
         shortucts=firestoredb.collection('shortcuts').stream()
         my_dict = {el.id: el.to_dict() for el in shortucts}
-        return my_dict, HTTPStatus.OK
+        result=[]
+        shortucts2=firestoredb.collection('shortcuts').stream()
+        for doc in shortucts2:
+            result.append(doc.to_dict())
+        return result, HTTPStatus.OK
 
     def post(self):
         shortcuts=firestoredb.collection('shortcuts')
