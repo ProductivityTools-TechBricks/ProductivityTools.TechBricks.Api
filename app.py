cd3ad7b667588f6fde6ac4fde8d84db21355883b
Pawel Wujczyk <pwujczyk@hotmail.com>
2022-05-16 22:43:35 +0200

    app

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_restful import Api
 from flask_migrate import Migrate
+from flask_cors  import CORS
 from extensions import db
 import os
 from config import Config
@@ -9,6 +10,7 @@ from Resources.Shortcut import ShortcutResource
 
 def create_app():
     app=Flask(__name__)
+    CORS(app)
     app.config.from_object(Config)
 
     #register_extensions(app)
