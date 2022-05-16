906090f8c427de6fb488c9351b29a1a9e53f3aa5
Pawel Wujczyk <pwujczyk@hotmail.com>
2022-05-16 21:18:41 +0200

    remove firestore to validate build

--- app.py
+++ app.py
@@ -27,6 +27,5 @@ def register_resources(app):
     api.add_resource(ShortcutResource, '/Card')
 
 if __name__=="__main__":
-
     app=create_app()
     app.run(port=int(os.environ.get("PORT", 8080)),host='0.0.0.0',debug=True)
--- extensions.py
+++ extensions.py
@@ -5,6 +5,6 @@ from firebase_admin import credentials, firestore, initialize_app
 # db=SQLAlchemy()
 
 #cred=credentials.Certificate("D:\\Bitbucket\\all.configuration\\pttechbricksapi-adminsdk.json")
-default_app=initialize_app()
-firestoredb=firestore.client()
+#default_app=initialize_app()
+#firestoredb=firestore.client()
 
