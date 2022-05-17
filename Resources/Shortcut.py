ece915aa508ed1019af8466c079643d771b03685
Pawel Wujczyk <pwujczyk@hotmail.com>
2022-05-17 06:37:22 +0200

    firestore db

--- Resources/Shortcut.py
+++ Resources/Shortcut.py
@@ -1,7 +1,27 @@
-from flask import request
+from flask import request,jsonify
+from flask_jwt_extended import jwt_required
 from flask_restful import Resource
 from http import HTTPStatus
+from extensions import firestoredb
+from firebase_admin import auth
 
 class ShortcutResource(Resource):
     def get(self):
-        return {'data':'pawel12'}, HTTPStatus.OK
+        # id_token=request.headers.environ['HTTP_AUTHORIZATION']
+        # id_token = id_token.replace("Bearer", "")
+        # id_token = id_token.replace(" ", "")
+        # decoded_token = auth.verify_id_token(id_token)
+
+        shortucts=firestoredb.collection('shortcuts').stream()
+        my_dict = {el.id: el.to_dict() for el in shortucts}
+        result=[]
+        shortucts2=firestoredb.collection('shortcuts').stream()
+        for doc in shortucts2:
+            partResult=doc.to_dict()
+            partResult["document_id"]=doc.id
+            result.append(partResult)
+        return result, HTTPStatus.OK
+
+    def post(self):
+        shortcuts=firestoredb.collection('shortcuts')
+        shortcuts.document().set(request.json)
--- extensions.py
+++ extensions.py
@@ -1,4 +1,9 @@
-from flask_sqlalchemy import SQLAlchemy
+from firebase_admin import credentials, firestore, initialize_app
+# from flask_sqlalchemy import SQLAlchemy
+#
+#
+# db=SQLAlchemy()
 
-
-db=SQLAlchemy()
+#cred=credentials.Certificate("D:\\Bitbucket\\all.configuration\\pttechbricksapi-adminsdk.json")
+#default_app=initialize_app()
+firestoredb=firestore.client()
