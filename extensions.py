d13c88941259770dd8713311c5ef392809892f05
Pawel Wujczyk <pwujczyk@hotmail.com>
2022-05-17 20:33:53 +0200

    try to remove file

--- extensions.py
+++ extensions.py
@@ -5,5 +5,5 @@ from firebase_admin import credentials, firestore, initialize_app
 # db=SQLAlchemy()
 
 cred=credentials.Certificate("pttechbricksapi-adminsdk.json")
-default_app=initialize_app(cred)
+default_app=initialize_app()
 firestoredb=firestore.client()
