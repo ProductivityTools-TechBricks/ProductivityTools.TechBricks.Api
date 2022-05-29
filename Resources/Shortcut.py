fdf3989890c8e4c6202b5ea9ebd1ead3a4cb518d
Pawel Wujczyk <pwujczyk@hotmail.com>
2022-05-29 17:01:37 +0200

    schema validation

--- Resources/Shortcut.py
+++ Resources/Shortcut.py
@@ -1,41 +1,54 @@
-from flask import request,jsonify
+from flask import request, jsonify
 from flask_expects_json import expects_json
 from flask_restful import Resource
 from http import HTTPStatus
 from extensions import firestoredb
 from firebase_admin import auth
 
+
 class ShortcutResource(Resource):
     def get(self):
-        id_token=request.headers.environ['HTTP_AUTHORIZATION']
+        id_token = request.headers.environ['HTTP_AUTHORIZATION']
         id_token = id_token.replace("Bearer", "")
         id_token = id_token.replace(" ", "")
 
-
-
-       # xx=auth.verify_id_token()
+        # xx=auth.verify_id_token()
         decoded_token = auth.verify_id_token(id_token)
 
-
-        shortucts=firestoredb.collection('shortcuts').stream()
+        shortucts = firestoredb.collection('shortcuts').stream()
         my_dict = {el.id: el.to_dict() for el in shortucts}
-        result=[]
-        shortucts2=firestoredb.collection('shortcuts').stream()
+        result = []
+        shortucts2 = firestoredb.collection('shortcuts').stream()
         for doc in shortucts2:
-            partResult=doc.to_dict()
-            partResult["document_id"]=doc.id
+            partResult = doc.to_dict()
+            partResult["document_id"] = doc.id
             result.append(partResult)
         return result, HTTPStatus.OK
 
     schema = {
         "type": "object",
         "properties": {
-            "name": {"xx": "string"},
-
+            "name": {
+                "type": "string"
+            },
+            "data": {
+                "type": "array",
+                "items": {
+                    "type": "object",
+                    "properties": {
+                        "key": {
+                            "type": "string"
+                        },
+                        "value": {
+                            "type": "string"
+                        }
+                    }
+                }
+            }
         }
     }
 
     @expects_json(schema)
     def post(self):
-        shortcuts=firestoredb.collection('shortcuts')
+        shortcuts = firestoredb.collection('shortcuts')
         shortcuts.document().set(request.json)
