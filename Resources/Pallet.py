b4e79d50273eecc7c2beafee5bb3c1d96b79319c
Pawel Wujczyk <pwujczyk@hotmail.com>
2022-06-04 06:45:53 +0200

    update pallet

--- Resources/Pallet.py
+++ Resources/Pallet.py
@@ -1,12 +1,12 @@
 from flask import request
 from flask_restful import Resource
 from flask_expects_json import expects_json
-
+from http import HTTPStatus
 from extensions import firestoredb
 
 class PalletResource(Resource):
 
-    schema={
+    addSchema={
         "type":"object",
         "properties":{
             "name":{
@@ -20,8 +20,52 @@ class PalletResource(Resource):
             }
         }
     }
+    updateSchema = {
+        "type": "object",
+        "properties": {
+            "name": {
+                "type": "string"
+            },
+            "owners": {
+                "type": "array",
+                "items": {
+                    "type": "string"
+                }
+            },
+            "bricks": {
+                "type": "array",
+                "items": {
+                    "type": "object",
+                    "properties": {
+                        "id": {
+                            "type": "string"
+                        },
+                        "key": {
+                            "type": "string"
+                        },
+                        "value": {
+                            "type": "string"
+                        }
+                    }
+                }
+            }
+        }
+    }
 
-    @expects_json(schema)
-    def post(self):
+    @expects_json(addSchema)
+    def put(self):
         palette = firestoredb.collection('pallet')
         palette.document().set(request.json)
+
+    def post(self):
+        document_id=request.json["document_id"]
+        firestoredb.collection('pallet').document(document_id).set(request.json)
+
+    def get(self):
+        result=[]
+        pallets=firestoredb.collection('pallet').stream();
+        for doc in pallets:
+            partResult=doc.to_dict();
+            partResult["document_id"]=doc.id
+            result.append(partResult)
+        return result,HTTPStatus.OK
